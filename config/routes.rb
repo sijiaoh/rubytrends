4afded806b5c206a98691bf0b9f87431683c3701
Sijia Wang <sijiaoh@outlook.com>
2021-12-27 00:01:21 +0900

    Add index page.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
