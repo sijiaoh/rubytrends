62bec8b2afdd9d49a40cd79bec83d7fc31aa7a8e
Sijia Wang <sijiaoh@outlook.com>
2021-12-29 06:55:50 +0900

    Remove unnecessary specs.

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -5,26 +5,6 @@ RSpec.describe "/users", type: :request do
     { terms_of_service: "1" }
   end
 
-  let(:invalid_attributes) do
-    { terms_of_service: "0" }
-  end
-
-  describe "GET /index" do
-    it "renders a successful response" do
-      User.create! valid_attributes
-      get users_url
-      expect(response).to be_successful
-    end
-  end
-
-  describe "GET /show" do
-    it "renders a successful response" do
-      user = User.create! valid_attributes
-      get user_url(user)
-      expect(response).to be_successful
-    end
-  end
-
   describe "GET /new" do
     it { expect { get new_user_url }.to raise_error(StandardError) }
   end
