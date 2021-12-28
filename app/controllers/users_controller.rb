644288c9325192c145361f8296cd72b84b129862
Sijia Wang <sijiaoh@outlook.com>
2021-12-28 18:18:23 +0900

    Raise error when access users/new without omniauth.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: %i[show]
+  before_action :set_omniauth_data, only: %i[new create]
 
   def index
     @users = User.all
@@ -12,10 +13,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    omniauth_data = session["devise.omniauth_data"]
-    raise "devise.omniauth_data not found." unless omniauth_data
-
-    @user = User.create_with_social_profile(user_params, omniauth_data)
+    @user = User.create_with_social_profile(user_params, @omniauth_data)
 
     if @user.save
       redirect_to user_url(@user), notice: "User was successfully created."
@@ -32,5 +30,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:terms_of_service)
+  end
+
+  def set_omniauth_data
+    omniauth_data = session["devise.omniauth_data"]
+    raise "devise.omniauth_data not found." unless omniauth_data
+
+    @omniauth_data = omniauth_data
   end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -10,12 +10,13 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should get new" do
-    get new_user_url
-    assert_response :success
+  test "should raise error when get new without omniauth" do
+    assert_raises StandardError do
+      get new_user_url
+    end
   end
 
-  test "should raise error in create user when without omniauth" do
+  test "should raise error when create user without omniauth" do
     assert_raises StandardError do
       post users_url, params: { user: { terms_of_service: "1" } }
     end
