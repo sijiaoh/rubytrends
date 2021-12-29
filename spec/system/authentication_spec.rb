66d5b9792cd6362d4f951cf9e203bf5bc8f0152d
Sijia Wang <sijiaoh@outlook.com>
2021-12-29 20:05:22 +0900

    Cosme specs.

--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -4,14 +4,14 @@ RSpec.describe "authentication", type: :system do
   it "sign up" do
     visit "/"
     click_on I18n.t("session.new.title")
-    expect(page).to have_current_path(sign_in_path)
+    expect(page).to have_current_path sign_in_path
     click_on I18n.t("session.new.with_google")
-    expect(page).to have_current_path(sign_up_path)
+    expect(page).to have_current_path sign_up_path
 
     expect do
       check User.human_attribute_name(:terms_of_service)
       click_on I18n.t("helpers.submit.create")
-      expect(page).not_to have_current_path(sign_up_path)
+      expect(page).not_to have_current_path sign_up_path
     end.to change(User, :count).by(1).and change(SocialProfile, :count).by(1)
 
     user = User.first
@@ -31,7 +31,7 @@ RSpec.describe "authentication", type: :system do
 
     visit sign_in_path
     click_on I18n.t("session.new.with_google")
-    expect(page).to have_current_path(root_path)
+    expect(page).to have_current_path root_path
     expect(page).to have_button I18n.t("sign_out")
   end
 
