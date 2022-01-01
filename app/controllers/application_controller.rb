d0bb1c244396b862bd865c03e7ff38ddadf19d42
Sijia Wang <sijiaoh@outlook.com>
2022-01-02 06:38:02 +0900

    Add authorization controll to application controller.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  private
+
+  def access_denied!
+    render_forbidden
+  end
+
+  def render_forbidden
+    render plain: "forbidden", status: :forbidden
+  end
+
+  def render_not_found
+    render plain: "not_found", status: :not_found
+  end
 end
