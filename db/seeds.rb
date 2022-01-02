e370c43e2c153871efedd9710f3b9b8168765db8
Sijia Wang <sijiaoh@outlook.com>
2022-01-03 04:07:35 +0900

    Add seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+return if %w[development test].include? ENV["RAILS_ENV"]
+
+data = [
+  {
+    gemname: "a",
+    existence_days: 4
+  },
+  {
+    gemname: "b",
+    existence_days: 1
+  },
+  {
+    gemname: "c",
+    existence_days: 6
+  }
+]
+
+data.each do |datum|
+  rubygem = Rubygem.create! name: datum[:gemname]
+
+  total_downloads = 0
+  datum[:existence_days].times do |count|
+    date = Time.zone.today.prev_day count + 1
+    daily_downloads = rand 100
+    total_downloads += daily_downloads
+    DailySummary.create!(
+      rubygem: rubygem,
+      date: date,
+      total_downloads: total_downloads,
+      daily_downloads: daily_downloads
+    )
+  end
+end
