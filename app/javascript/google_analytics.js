ac1ff53df9f4da6626cf51407289775a30e5c9d4
Sijia Wang <sijiaoh@outlook.com>
2022-01-05 10:52:54 +0900

    Fix error that occurs when GA_MEASUREMENT_ID is not set.

--- app/javascript/google_analytics.js
+++ app/javascript/google_analytics.js
@@ -1,5 +1,5 @@
 document.documentElement.addEventListener("turbo:load", () => {
-  gtag("event", "page_view", {
+  window.gtag?.("event", "page_view", {
     page_title: document.title,
     page_location: location.href,
     page_path: location.pathname,
