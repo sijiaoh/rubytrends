a6ab424e0aeeb4a46b49c812d8bc9630293f4132
Sijia Wang <sijiaoh@outlook.com>
2022-01-05 10:41:15 +0900

    Add test to Rubygem#fetch_if_need!.

--- spec/models/rubygem_spec.rb
+++ spec/models/rubygem_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe Rubygem, type: :model do
     end
 
     context "with exists data" do
-      let(:new_data_count) { 5 }
-
       before do
         allow(rubygem).to receive(:fetch_source_data).and_return(source_data[new_data_count..])
         rubygem.fetch_if_need!
@@ -29,10 +27,26 @@ RSpec.describe Rubygem, type: :model do
         allow(rubygem).to receive(:fetch_source_data).and_return(source_data)
       end
 
-      it "creates all daily summaries" do
-        expect do
+      context "with 5 records" do # rubocop:disable RSpec/NestedGroups
+        let(:new_data_count) { 5 }
+
+        it "creates all daily summaries" do
+          expect do
+            rubygem.fetch_if_need!
+          end.to change(DailySummary, :count).by(new_data_count)
+        end
+      end
+
+      context "with a record" do # rubocop:disable RSpec/NestedGroups
+        let(:new_data_count) { 1 }
+
+        it "calculates daily_download correctly" do
           rubygem.fetch_if_need!
-        end.to change(DailySummary, :count).by(new_data_count)
+          daily_summaries = DailySummary.order(date: :desc).limit(2)
+          first = daily_summaries.first
+          second = daily_summaries.second
+          expect(first.daily_downloads).to eq(first.total_downloads - second.total_downloads)
+        end
       end
     end
   end
