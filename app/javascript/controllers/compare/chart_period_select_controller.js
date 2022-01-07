d9788da2f384e31342d2e3acd958a3fa8406a712
Sijia Wang <sijiaoh@outlook.com>
2022-01-07 12:54:03 +0900

    Add loading to chart.

--- app/javascript/controllers/compare/chart_period_select_controller.js
+++ app/javascript/controllers/compare/chart_period_select_controller.js
@@ -4,6 +4,10 @@ export default class extends Controller {
   static targets = ["select"];
   static values = { path: String };
 
+  connect() {
+    this.change();
+  }
+
   change() {
     const path = this.pathValue.replace(":period", this.selectTarget.value);
     const chartElm = document.getElementById("chart");
