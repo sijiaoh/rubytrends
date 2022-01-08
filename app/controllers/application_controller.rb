9296e9c4477d1df12cc79e2d08581930d3ec8f92
Sijia Wang <sijiaoh@outlook.com>
2022-01-08 10:04:32 +0900

    Remove authority managements.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
-
   private
-
-  def access_denied!
-    render_forbidden
-  end
-
-  def render_forbidden
-    render plain: "forbidden", status: :forbidden
-  end
 
   def render_not_found
     render plain: "not_found", status: :not_found
