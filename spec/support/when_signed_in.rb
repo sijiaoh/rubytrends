2be8f51acf83a4afb0615d0a22820eccde330616
Sijia Wang <sijiaoh@outlook.com>
2022-01-09 06:46:12 +0900

    Refactor tests.

---
+++ spec/support/when_signed_in.rb
@@ -0,0 +1,7 @@
+shared_context "when signed in" do
+  let(:current_user) { create :user }
+
+  before do
+    sign_in current_user
+  end
+end
--- spec/system/authentication_spec.rb
+++ spec/system/authentication_spec.rb
@@ -1,48 +1,53 @@
 require "rails_helper"
 
 RSpec.describe "authentication", type: :system do
-  it "sign up" do
-    visit "/"
-    click_on I18n.t("session.new.title")
-    expect(page).to have_current_path sign_in_path
-    click_on I18n.t("session.new.with_google")
-    expect(page).to have_current_path sign_up_path
+  describe "sign up" do
+    it "creates new user and signs in" do
+      visit "/"
+      click_on I18n.t("session.new.title")
+      expect(page).to have_current_path sign_in_path
+      click_on I18n.t("session.new.with_google")
+      expect(page).to have_current_path sign_up_path
 
-    expect do
-      check User.human_attribute_name(:terms_of_service)
-      click_on I18n.t("helpers.submit.create")
-      expect(page).not_to have_current_path sign_up_path
-    end.to change(User, :count).by(1).and change(SocialProfile, :count).by(1)
+      expect do
+        check User.human_attribute_name(:terms_of_service)
+        click_on I18n.t("helpers.submit.create")
+        expect(page).not_to have_current_path sign_up_path
+      end.to change(User, :count).by(1).and change(SocialProfile, :count).by(1)
 
-    user = User.first
-    social_profile = user.social_profiles.first
-    google = OmniAuth.config.mock_auth[:google_oauth2]
-    expect(social_profile.provider).to eq google[:provider]
-    expect(social_profile.uid).to eq google[:uid]
-    expect(social_profile.email).to eq google[:info][:email]
+      user = User.first
+      social_profile = user.social_profiles.first
+      google = OmniAuth.config.mock_auth[:google_oauth2]
+      expect(social_profile.provider).to eq google[:provider]
+      expect(social_profile.uid).to eq google[:uid]
+      expect(social_profile.email).to eq google[:info][:email]
 
-    expect(page).to have_button I18n.t("sign_out")
+      expect(page).to have_button I18n.t("sign_out")
+    end
   end
 
-  it "sign in" do
-    google = OmniAuth.config.mock_auth[:google_oauth2]
-    user = User.build_with_social_profile({}, google)
-    user.save!
+  describe "sign in" do
+    it "signs in" do
+      google = OmniAuth.config.mock_auth[:google_oauth2]
+      user = User.build_with_social_profile({}, google)
+      user.save!
 
-    visit sign_in_path
-    click_on I18n.t("session.new.with_google")
-    expect(page).to have_current_path root_path
-    expect(page).to have_button I18n.t("sign_out")
+      visit sign_in_path
+      click_on I18n.t("session.new.with_google")
+      expect(page).to have_current_path root_path
+      expect(page).to have_button I18n.t("sign_out")
+    end
   end
 
-  it "sign out" do
-    user = create :user
-    sign_in user
+  describe "sign out" do
+    include_context "when signed in"
 
-    visit root_path
-    expect(page).to have_button I18n.t("sign_out")
+    it "signs out" do
+      visit root_path
+      expect(page).to have_button I18n.t("sign_out")
 
-    click_on I18n.t("sign_out")
-    expect(page).not_to have_button I18n.t("sign_out")
+      click_on I18n.t("sign_out")
+      expect(page).not_to have_button I18n.t("sign_out")
+    end
   end
 end
