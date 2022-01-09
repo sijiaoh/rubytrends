1b8718b1b23990c218a40503b360b51bf710140d
Sijia Wang <sijiaoh@outlook.com>
2022-01-10 03:20:31 +0900

    Customize tinymce.

--- app/javascript/controllers/wysiwyg_controller.js
+++ app/javascript/controllers/wysiwyg_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus";
 import { v4 } from "uuid";
 
 export default class extends Controller {
-  connect() {
+  async connect() {
     const textareas = this.element.getElementsByTagName("textarea");
     if (textareas.length != 1)
       throw new Error("wysiwyg_controller shuld have 1 textarea");
@@ -11,7 +11,44 @@ export default class extends Controller {
     const id = v4();
     textarea.id = id;
     this.selector = `#${id}`;
-    tinymce.init({ selector: this.selector });
+
+    await tinymce.init({
+      selector: this.selector,
+      menubar: false,
+      statusbar: false,
+
+      setup: this.editorSetup,
+
+      plugins: ["autoresize", "table"],
+      min_height: 500,
+      autoresize_bottom_margin: 0,
+
+      toolbar: [
+        "undo",
+        "redo",
+        "|",
+        "h2",
+        "h3",
+        "alignleft",
+        "aligncenter",
+        "alignright",
+        "|",
+        "bold",
+        "italic",
+        "|",
+        "table",
+      ].join(" "),
+    });
+  }
+
+  editorSetup(editor) {
+    editor.on("keydown", function (event) {
+      // Always use p tag to newline.
+      const enter = 13;
+      if (event.keyCode == enter && event.shiftKey) {
+        event.shiftKey = false;
+      }
+    });
   }
 
   disconnect() {
