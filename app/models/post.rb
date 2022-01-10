57f47838f85f4fab439672e9be0f39c5db65c33d
Sijia Wang <sijiaoh@outlook.com>
2022-01-10 11:50:56 +0900

    Escape content before post save.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ApplicationRecord
+  before_save :escape_content
+
   belongs_to :user
+
+  private
+
+  def escape_content
+    self.content = Loofah.fragment(content).scrub!(:escape).to_s
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require "rails_helper"
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "escapes content before save" do
+    content = "<script>alert('hi');</script>"
+    post = create :post, { user: create(:user), content: }
+    expect(post.content).to eq "&lt;script&gt;alert('hi');&lt;/script&gt;"
+  end
 end
