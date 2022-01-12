21318f2abd6a9d222e7adb2b6687479fe523a11e
Sijia Wang <sijiaoh@outlook.com>
2022-01-13 07:27:42 +0900

    Refactor views.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[show edit update destroy]
 
   def index
-    @posts = policy_scope(Post).all
+    @posts = policy_scope(Post).all.includes([:user])
     authorize @posts
   end
 
