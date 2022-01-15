26823a77aa318823aab309e79fd0b038d288469f
Sijia Wang <sijiaoh@outlook.com>
2022-01-15 21:22:17 +0900

    Fix syntax error?
    Seems ruby 3.1.0 error?

--- app/models/query_count.rb
+++ app/models/query_count.rb
@@ -2,7 +2,7 @@ class QueryCount < ApplicationRecord
   def self.count_query!(query)
     query = query.split("~").uniq.sort.join("~")
 
-    query_count = find_by query: # rubocop:disable Lint/UselessAssignment
+    query_count = find_by(query:)
     if query_count.present?
       query_count.increment! :count # rubocop:disable Rails/SkipsModelValidations
     else
