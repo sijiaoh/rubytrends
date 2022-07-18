9e495728bea8f47fe42b79851d558fea4db97080
sijiaoh <sijiaoh@outlook.com>
2022-07-18 09:53:44 +0900

    Refactor scaffold generator.

--- lib/generators/ellie/scaffold/scaffold_generator.rb
+++ lib/generators/ellie/scaffold/scaffold_generator.rb
@@ -1,29 +1,12 @@
 require "rails/generators/erb/scaffold/scaffold_generator"
-require "rails/generators/resource_helpers"
 
 module Ellie
   module Generators
     class ScaffoldGenerator < Erb::Generators::ScaffoldGenerator
-      include Rails::Generators::ResourceHelpers
-
       source_root File.expand_path("templates", __dir__)
-
-      argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
       def create_root_folder
         empty_directory File.join("app/views", controller_file_path)
-      end
-
-      def views
-        paths = Dir[File.join(File.expand_path("templates", __dir__), "/*")]
-        names = paths.map do |path|
-          File.basename(path, ".html.erb.tt")
-        end
-
-        reject_list = %w[index_partial partial]
-        names.reject do |name|
-          reject_list.include? name
-        end
       end
 
       def copy_view_files
@@ -45,8 +28,16 @@ module Ellie
 
       private
 
-      def available_views
-        %w[index edit show new _form]
+      def views
+        paths = Dir[File.join(File.expand_path("templates", __dir__), "/*")]
+        names = paths.map do |path|
+          File.basename(path, ".html.erb.tt")
+        end
+
+        reject_list = %w[index_partial partial]
+        names.reject do |name|
+          reject_list.include? name
+        end
       end
     end
   end
