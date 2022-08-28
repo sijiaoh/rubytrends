cb8c95aaae596a416227ddf2ce4be510c51bd63f
sijiaoh <sijiaoh@outlook.com>
2022-08-28 21:53:11 +0900

    Remove devise comment.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,10 +10,6 @@
 class User < ApplicationRecord
   include HashidSluggable
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :validatable
   devise :omniauthable, omniauth_providers: [:google_oauth2]
   rolify
 
