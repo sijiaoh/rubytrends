d99f72624860962a8ad5f584ceff212f6936a0b9
sijiaoh <sijiaoh@outlook.com>
2022-08-30 07:06:48 +0900

    Fix loading icon render text before load image.
    Use svg instead.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def ga_measurement_id
     Utils.env_or_credential "ga_measurement_id", ""
   end
+
+  def svg(path, options = {})
+    content_tag :div, class: options[:class] do
+      Rails.root.join("app", "assets", "images", path).read.html_safe # rubocop:disable Rails/OutputSafety
+    end
+  end
 end
