46bd93f2ed291ec74abafed9465d1da3acfd4dfe
sijiaoh <sijiaoh@outlook.com>
2022-11-23 22:06:07 +0900

    Add wysiwyg_viewer helper instead of shared partial.

--- app/helpers/wysiwyg_helper.rb
+++ app/helpers/wysiwyg_helper.rb
@@ -7,4 +7,8 @@ module WysiwygHelper
   def prism_theme_css
     "https://cdnjs.cloudflare.com/ajax/libs/prism-themes/1.9.0/prism-dracula.min.css"
   end
+
+  def wysiwyg_viewer(content)
+    content_tag :div, content.html_safe, "data-controller": "wysiwyg-viewer", class: wysiwyg_class # rubocop:disable Rails/OutputSafety
+  end
 end
