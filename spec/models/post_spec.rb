6f2fa703b09b02728da7a3000ffc06a7b3ea9460
sijiaoh <sijiaoh@outlook.com>
2022-11-27 19:00:04 +0900

    Add has content concern shared_example.

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -39,32 +39,5 @@ describe Post do
     end
   end
 
-  describe "content escaping" do
-    let(:content) { "<script>alert('hi');</script>" }
-    let(:post) { create(:post, user: create(:user), content:, editor_type:) }
-
-    context "when editor_type is textarea" do
-      let(:editor_type) { :textarea }
-
-      it "does not escape content before save" do
-        expect(post.content).to eq content
-      end
-    end
-
-    context "when editor_type is wysiwyg" do
-      let(:editor_type) { :wysiwyg }
-
-      it "escapes content before save" do
-        expect(post.content).to eq "&lt;script&gt;alert('hi');&lt;/script&gt;"
-      end
-    end
-
-    context "when editor_type is markdown" do
-      let(:editor_type) { :markdown }
-
-      it "does not escape content before save" do
-        expect(post.content).to eq content
-      end
-    end
-  end
+  include_examples "has content concern"
 end
