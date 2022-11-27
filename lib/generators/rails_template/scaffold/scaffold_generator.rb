071b3a414cdf42d93df1a644c3853c2b300857fe
sijiaoh <sijiaoh@outlook.com>
2022-11-27 15:19:50 +0900

    Refactor scaffold template links.

--- lib/generators/rails_template/scaffold/scaffold_generator.rb
+++ lib/generators/rails_template/scaffold/scaffold_generator.rb
@@ -22,14 +22,18 @@ module RailsTemplate
         template "partial.html.erb", File.join("app/views", controller_file_path, "_#{singular_table_name}.html.erb")
       end
 
-      def view_i18n_scope
-        @view_i18n_scope ||= route_url.tr("/", ".").gsub(/\A./, "")
+      def copy_helper_file
+        template "helper.rb.erb.tt", File.join("app/helpers", "#{controller_file_path}_helper.rb")
       end
 
       private
 
+      def view_i18n_scope
+        @view_i18n_scope ||= route_url.tr("/", ".").gsub(/\A./, "")
+      end
+
       def views
-        paths = Dir[File.join(File.expand_path("templates", __dir__), "/*")]
+        paths = Dir[File.join(File.expand_path("templates", __dir__), "/*.html.erb.tt")]
         names = paths.map do |path|
           File.basename(path, ".html.erb.tt")
         end
